c29b2390768146e06a9c36fe4fa7d8b417034440
roxlmz <79018965+roxlmz@users.noreply.github.com>
2022-01-10 08:09:16 -0300

    rhoxlmz

--- js/rh0x.js
+++ js/rh0x.js
@@ -1,13 +1,17 @@
+function slideUpSobre() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    document.getElementById("sobre").className = "slideUp";
+  }
+}
 function clicar(){
   const hambegerBtn = document.getElementsByClassName("btn-hamburger")[0]
   const navLinks = document.getElementsByClassName("menu")[0]
     navLinks.classList.toggle("active")
 }
 function MostarMais() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("mostrar");
-        
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("mostrar");
     if (dots.style.display === "none") {
         dots.style.display = "inline";
         btnText.innerHTML = "Mostrar +"; 
@@ -17,5 +21,5 @@ function MostarMais() {
             btnText.innerHTML = "Mostrar -"; 
             moreText.style.display = "inline";
           }
-        }
+}
 
